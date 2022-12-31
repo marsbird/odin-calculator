b8ddbe82fb0c2525bbf6ae0ced1593b3a2d4a282
Marselena <github.seaplanes@simplelogin.co>
2022-12-31 18:18:34 -0500

    Change "result" to "display" in js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// get result field and set default to 0
-const result = document.querySelector(".result");
-result.textContent = "0";
+// get display field and set default to 0
+const display = document.querySelector(".display");
 
 // operator functions
 function add(x, y) {
