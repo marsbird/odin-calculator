088811e3e036c56621a8e3e4e938bfbdd1efaf31
Marselena <github.seaplanes@simplelogin.co>
2022-12-30 23:24:25 -0500

    Write operator functions

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 const result = document.querySelector(".result");
 result.textContent = "0";
 
+// operator functions
+function add(x, y) {
+  return x + y;
+}
+function subtract(x, y) {
+  return x - y;
+}
+function multiply(x, y) {
+  return x * y;
+}
+function divide(x, y) {
+  return x / y;
+}
